82d3610b22865479431e1146a95218d3476c9d01
Ruan Pablo <ruanpablo@MacBook-Air-de-Ruan.local>
2025-08-12 22:24:33 -0300

    Atualiza links do repositório nos metadados

--- src/meta.js
+++ src/meta.js
@@ -7,7 +7,7 @@
 // @license      MPL-2.0
 // @homepageURL  https://github.com/rm0ntoya/wgram-wplace
 // @supportURL   https://github.com/rm0ntoya/wgram-wplace/issues
-// @icon         https://raw.githubusercontent.com/rm0ntoya/wgram-wplace/main/dist/icon.png
+// @icon         https://raw.githubusercontent.com/rm0ntoya/wgram-wplace/refs/heads/main/src/assets/icon.png
 
 // @match        *://*.wplace.live/*
 
