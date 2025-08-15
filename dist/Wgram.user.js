6e076c86c1b22603fab87a19dbc4e28aa24c2070
Ruan Pablo <ruanpablo@MacBook-Air-de-Ruan.local>
2025-08-15 20:51:08 -0300

    Corrigido Bug de Nao conseguir copiar ID das cordenadas

--- dist/Wgram.user.js
+++ dist/Wgram.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wgram
 // @namespace    https://github.com/rm0ntoya
-// @version      1.8.1
+// @version      1.9.1
 // @description  Um script de usuário para carregar templates e partilhar coordenadas do WGram com captura aprimorada e modo manutenção.
 // @author       rm0ntoya
 // @license      MPL-2.0
