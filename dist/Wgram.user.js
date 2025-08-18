f7945abd1e0a96811d88029ff0cf06e9069e13af
Ruan Pablo <ruanpablo@MacBook-Air-de-Ruan.local>
2025-08-17 22:00:34 -0300

    Sistema de usar varias contas no mesmo navegador ativado

--- dist/Wgram.user.js
+++ dist/Wgram.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wgram
 // @namespace    https://github.com/rm0ntoya
-// @version      2.0.5
+// @version      1.8.6
 // @description  Um script de usuário para carregar templates, partilhar coordenadas e gerenciar o localStorage no WGram.
 // @author       rm0ntoya
 // @license      MPL-2.0
