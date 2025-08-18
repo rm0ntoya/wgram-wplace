691a60c7f67566f88d9e58b0e7e70dce2c758b3b
Ruan Pablo <ruanpablo@MacBook-Air-de-Ruan.local>
2025-08-18 14:39:40 -0300

    Nova Versao

--- dist/Wgram.user.js
+++ dist/Wgram.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wgram - Pixel Art Manager
 // @namespace    https://github.com/rm0ntoya
-// @version      2.0.0
+// @version      1.9.3
 // @description  Um script de usuário para carregar templates, partilhar coordenadas e gerenciar o localStorage no WGram, agora com sincronização de contas.
 // @author       rm0ntoya & Gemini
 // @license      MPL-2.0
